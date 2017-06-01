a5646bdaa718532d861f351ed5d4d6a15fd219cf
timvisee <timvisee@gmail.com>
2017-06-01 16:16:03 +0200

    Create Core class

--- server/Core.js
+++ server/Core.js
@@ -20,10 +20,69 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.                *
  ******************************************************************************/
 
-// Set some default jQuery Mobile settings for the current application.
-$(document).bind("mobileinit", function(){
-    // Set the default page transition
-    $.extend($.mobile, {
-        defaultPageTransition: 'slide'
-    });
-});
+/**
+ * Core class.
+ *
+ * @class
+ * @constructor
+ */
+var Core = function() {};
+
+/**
+ * Model manager root.
+ *
+ * @type {Object}
+ */
+Core.model = {};
+
+/**
+ * User model manager.
+ *
+ * @type {UserModelManager|null} User model manager, or null if it isn't instantiated yet.
+ */
+Core.model.userModelManager = null;
+
+/**
+ * Session model manager.
+ *
+ * @type {SessionModelManager|null} Session model manager, or null if it isn't instantiated yet.
+ */
+Core.model.sessionModelManager = null;
+
+/**
+ * HTTP(S) server instance.
+ *
+ * @type {*} Server instance.
+ */
+Core.server = null;
+
+/**
+ * Express app instance.
+ *
+ * @type {*} Express app.
+ */
+Core.expressApp = null;
+
+/**
+ * Router instance.
+ *
+ * @type {Router}
+ */
+Core.router = null;
+
+/**
+ * Real time server instance.
+ *
+ * @type {RealTime}
+ */
+Core.realTime = null;
+
+/**
+ * Event loop monitor.
+ *
+ * @type {EventLoopMonitor}
+ */
+Core.eventLoopMonitor = null;
+
+// Export the class
+module.exports = Core;
