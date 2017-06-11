ec1926ac30aebcae5a85774247db9cd9618c5a3d
timvisee <timvisee@gmail.com>
2017-06-11 14:10:22 +0200

    Translate the login required page to Dutch

--- server/app/router/middleware/SessionValidator.js
+++ server/app/router/middleware/SessionValidator.js
@@ -54,7 +54,7 @@ SessionValidator.route = function(req, res, next) {
      * This renders the page that shows the client he's required to login.
      */
     req.showRequireLoginPage = function() {
-        LayoutRenderer.render(req, res, next, 'requirelogin', 'Whoops!', {
+        LayoutRenderer.render(req, res, next, 'requirelogin', 'Oeps!', {
             next: encodeURIComponent(req.originalUrl)
         });
     };
