0874dcbf6a8069935120a6a4c56f01a418113dd2
timvisee <timvisee@gmail.com>
2017-06-13 18:36:01 +0200

    Create brief game overview page on dashboard

--- server/app/route/index.js
+++ server/app/route/index.js
@@ -53,9 +53,46 @@ router.get('/', function(req, res, next) {
         LayoutRenderer.render(req, res, next, 'index', appInfo.APP_NAME, pageOptions);
         return;
     }
+    // Create a callback latch
+    var latch = new CallbackLatch();
 
-    // Render the dashboard
-    LayoutRenderer.render(req, res, next, 'dashboard', appInfo.APP_NAME);
+    // Create an object with layout options
+    var options = {
+        games: {
+            games: {
+                open: [],
+                active: []
+            }
+        }
+    };
+
+    // Count the games
+    latch.add(2);
+    getGameList(0, 3, function(err, games) {
+        // Call back errors
+        if(err !== null)
+            next(err);
+        else
+            options.games.games.open = games;
+
+        // Resolve the latch
+        latch.resolve();
+    });
+    getGameList(1, 3, function(err, games) {
+        // Call back errors
+        if(err !== null)
+            next(err);
+        else
+            options.games.games.active = games;
+
+        // Resolve the latch
+        latch.resolve();
+    });
+
+    // Render the games page
+    latch.then(function() {
+        LayoutRenderer.render(req, res, next, 'dashboard', appInfo.APP_NAME, options);
+    });
 });
 
 // Ajax requests
@@ -83,6 +120,105 @@ router.use('/about', about);
 router.use('/create', create);
 
 // TODO: Add (back) status route
+
+/**
+ * Get the game list.
+ *
+ * @param {Number} stage Game stage.
+ * @param {Number|undefined} limit Limit of games to fetch, undefined to fetch all.
+ * @param {function} callback Callback(err, games)
+ */
+function getGameList(stage, limit, callback) {
+    // Create a list of usable game objects to use for rendering
+    var gameObjects = [];
+
+    // Create a callback latch to determine whether to start rendering the layout
+    var latch = new CallbackLatch();
+
+    // Define whether we called back
+    var calledBack = false;
+
+    // Get the list of active games
+    latch.add();
+    Core.model.gameModelManager.getGamesWithStage(stage, {
+        limit
+    }, function(err, games) {
+        // Call back errors
+        if(err !== null) {
+            callback(err);
+            return;
+        }
+
+        // Fill the list of game objects
+        games.forEach(function(game) {
+            // Add a callback latch for this entry
+            latch.add();
+
+            // Create a dummy game object
+            var gameObject = {
+                id: game.getIdHex(),
+                name: null
+            };
+
+            // Create a callback latch that is used to fetch game data
+            var gameDataLatch = new CallbackLatch();
+
+            // Get the game name and put it in the object
+            gameDataLatch.add();
+            game.getName(function(err, name) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        callback(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the name
+                gameObject.name = name;
+
+                // Resolve the latch
+                gameDataLatch.resolve();
+            });
+
+            // Get the player count for this game
+            gameDataLatch.add();
+            Core.model.gameUserModelManager.getGameUserCount(game, function(err, count) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        callback(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the user count
+                gameObject.userCount = count;
+
+                // Resolve the latch
+                gameDataLatch.resolve();
+            });
+
+            // Push the object in the game objects array when we're done
+            gameDataLatch.then(function() {
+                // Add the game object to the list
+                gameObjects.push(gameObject);
+
+                // Resolve the latch
+                latch.resolve();
+            });
+        });
+
+        // Resolve the latch
+        latch.resolve();
+    });
+
+    // Call back the list
+    latch.then(function() {
+        if(!calledBack)
+            callback(null, gameObjects);
+    });
+}
 
 // Export the router
 module.exports = router;
