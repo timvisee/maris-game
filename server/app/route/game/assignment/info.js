3bd9aa98bfc1f36051ae98d80f24b90c3f58d392
timvisee <timvisee@gmail.com>
2017-06-19 23:43:02 +0200

    Create assignment info page

---
+++ server/app/route/game/assignment/info.js
@@ -0,0 +1,205 @@
+/******************************************************************************
+ * Copyright (c) Maris Game 2017. All rights reserved.                        *
+ *                                                                            *
+ * @author Tim Visee                                                          *
+ * @website http://timvisee.com/                                              *
+ *                                                                            *
+ * Open Source != No Copyright                                                *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * You should have received a copy of The MIT License (MIT) along with this   *
+ * program. If not, see <http://opensource.org/licenses/MIT/>.                *
+ ******************************************************************************/
+
+var crypto = require('crypto');
+
+var Core = require('../../../../Core');
+var LayoutRenderer = require('../../../layout/LayoutRenderer');
+var CallbackLatch = require('../../../util/CallbackLatch');
+
+// Export the module
+module.exports = {
+
+    /**
+     * Route the player pages.
+     *
+     * @param router Express router instance.
+     */
+    route: (router) => {
+        // Store the module instance
+        const self = module.exports;
+
+        // Route the points list
+        router.get('/:game/point/:point', self.get);
+    },
+
+    /**
+     * Get page.
+     *
+     * @param req Express request object.
+     * @param res Express response object.
+     * @param next Express next callback.
+     */
+    get: (req, res, next) => {
+        // Make sure the user has a valid session
+        if(!req.requireValidSession())
+            return;
+
+        // Get the game, user and assignment
+        const game = req.game;
+        const user = req.session.user;
+        const assignment = req.assignment;
+
+        // Call back if the game is invalid
+        if(game === undefined) {
+            next(new Error('Ongeldig spel.'));
+            return;
+        }
+
+        // Call back if the assignment is invalid
+        if(assignment === undefined) {
+            next(new Error('Ongeldig opdracht.'));
+            return;
+        }
+
+        // The player must have the ability to manage this game
+        game.hasManagePermission(user, function(err, hasPermission) {
+            // Handle errors
+            if(err !== null) {
+                next(err);
+                return;
+            }
+
+            // Handle no permission situations
+            if(!hasPermission) {
+                LayoutRenderer.render(req, res, next, 'nopermission', 'Oeps!');
+                return;
+            }
+
+            // Create the page options object
+            var options = {
+                page: {
+                    leftButton: 'back'
+                },
+                assignment: {
+                    name: '',
+                    description: '',
+                    answer_text: false,
+                    answer_file: false,
+                    retry: false
+                }
+            };
+
+            // Create a callback latch for the games properties
+            var latch = new CallbackLatch();
+            var calledBack = false;
+
+            // Fetch the name
+            latch.add();
+            assignment.getName(function(err, name) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        next(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the property
+                options.assignment.name = name;
+
+                // Resolve the latch
+                latch.resolve();
+            });
+
+            // Fetch the description
+            latch.add();
+            assignment.getDescription(function(err, description) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        next(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the property
+                options.assignment.description = description;
+
+                // Resolve the latch
+                latch.resolve();
+            });
+
+            // Fetch the text answer state
+            latch.add();
+            assignment.getAnswerText(function(err, answerText) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        next(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the property
+                options.assignment.answer_text = answerText;
+
+                // Resolve the latch
+                latch.resolve();
+            });
+
+            // Fetch the file answer state
+            latch.add();
+            assignment.getAnswerFile(function(err, answerFile) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        next(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the property
+                options.assignment.answer_file = answerFile;
+
+                // Resolve the latch
+                latch.resolve();
+            });
+
+            // Fetch the retry state
+            latch.add();
+            assignment.isRetry(function(err, retry) {
+                // Call back errors
+                if(err !== null) {
+                    if(!calledBack)
+                        next(err);
+                    calledBack = true;
+                    return;
+                }
+
+                // Set the property
+                options.assignment.retry = retry;
+
+                // Resolve the latch
+                latch.resolve();
+            });
+
+            // Render the page when we're ready
+            latch.then(function() {
+                // Render the game page if we didn't call back yet
+                if(!calledBack)
+                    LayoutRenderer.render(req, res, next, 'gameassignmentinfo', options.assignment.name, options);
+                calledBack = true;
+            });
+        });
+    },
+};
